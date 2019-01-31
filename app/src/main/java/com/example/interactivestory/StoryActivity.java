e57fa68cda1e2a4f9ea223832bd1843ac2a233ef
Georgeana Dias <geanamonte@gmail.com>
2019-01-31 13:32:46 -0200

    Remoção da classe StoryActivity e criação de uma Empty Activity com o nome StoryActivity. Leitura do parâmetro name do intent

--- app/src/main/java/com/example/interactivestory/StoryActivity.java
+++ app/src/main/java/com/example/interactivestory/StoryActivity.java
@@ -1,4 +1,20 @@
 package com.example.interactivestory;
 
-public class StoryActivity {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StoryActivity extends AppCompatActivity {
+    public static final String TAG = StoryActivity.class.getSimpleName();
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        intent.getStringExtra("name");
+
+
+    }
 }
