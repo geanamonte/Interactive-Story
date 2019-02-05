f60da2c4c3d572ffb15df5bc83d6ce01333de8ff
Georgeana Dias <geanamonte@gmail.com>
2019-02-05 17:13:45 -0200

    Criação do sistema de passagem de páginas

--- app/src/main/java/com/example/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/example/interactivestory/ui/StoryActivity.java
@@ -1,25 +1,95 @@
 package com.example.interactivestory.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.interactivestory.R;
+import com.example.interactivestory.model.Choice;
+import com.example.interactivestory.model.Page;
+import com.example.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
     public static final String TAG = StoryActivity.class.getSimpleName();
+    private Story story;
+    private String name;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
 
+        storyImageView = (ImageView) findViewById(R.id.storyImageView);
+        storyTextView = (TextView) findViewById(R.id.storyTextView);
+        choice1Button = (Button) findViewById(R.id.choice1button);
+        choice2Button = (Button) findViewById(R.id.choice2button);
+
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        name = intent.getStringExtra(getString(R.string.key_name));
         if (name == null || name.isEmpty()){
             name = "Friend";
         }
 
+        Log.d(TAG,name);
+        story = new Story();
+        loadPage(0);
 
+    }
+
+    private void loadPage(int pageNumber) {
+        final Page page = story.getPage(pageNumber);
+
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
+
+        String pageText = getString(page.getTextId());
+        pageText = String.format(pageText,name);
+        storyTextView.setText(pageText);
+
+        if(page.isFinalPage()) {
+            choice1Button.setVisibility(View.INVISIBLE);
+            choice2Button.setText(getString(R.string.play_again));
+            choice2Button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loadPage(0);
+                }
+            });
+        }
+        else{
+            loadButtons(page);
+        }
+
+    }
+
+    private void loadButtons(final Page page) {
+        choice1Button.setVisibility(View.VISIBLE);
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadPage(page.getChoice1().getNextPage());
+            }
+        });
+        choice2Button.setText(page.getChoice2().getTextId());
+        choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadPage(page.getChoice2().getNextPage());
+            }
+        });
     }
 }
